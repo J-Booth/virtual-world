2397d678fc0d7086a963891364680fcffdb5b9a0
Joshua Booth <joshb00th@icloud.com>
2017-07-16 13:06:35 +1200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,3 +6,15 @@ from tkinter import ttk
 import os
 import re
 import sys
+
+# Fonts
+LARGE_FONT = ("Verdana", 25)
+MEDIUM_FONT = ("Verdana", 12)
+SMALL_FONT = ("Verdana", 8)
+
+# Data Files
+options_file = 'options.txt'
+current_user_file = 'current_user.txt'
+user_data_file = 'user_data.txt'
+user_names_file = 'user_names.txt'
+COFFEE_DATA_F = 'coffee_data.txt'
