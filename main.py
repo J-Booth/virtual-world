f8aa5b7669324c4bcd4b35be862f745259374542
Joshua Booth <joshb00th@icloud.com>
2017-07-16 13:02:42 +1200

    Update main.py

--- main.py
+++ main.py
@@ -776,6 +776,11 @@ class ShopPage(tk.Frame):
 
 
 class CoffeeShopPage(tk.Frame):
+    cappu_cost = 3.50
+    espre_cost = 3.00
+    flatw_cost = 2.50
+    latte_cost = 4.50
+    mocha_cost = 3.50
 
     def __init__(self, parent, controller):
         """ Coffee Shop where the user can buy different coffees """
@@ -816,7 +821,8 @@ class CoffeeShopPage(tk.Frame):
                                           font=MEDIUM_FONT)
         self.cappuccino_label.grid(row=14, column=1, columnspan=5, pady=10,
                                    sticky="W")
-        self.cappuccino_price = ttk.Label(self, text="$3.00", font=MEDIUM_FONT)
+        self.cappuccino_price = ttk.Label(self, font=MEDIUM_FONT,
+                                          text="${:.2f}".format(self.cappu_cost))
         self.cappuccino_price.grid(row=14, column=4, columnspan=3, pady=10)
         cappuccino_vcmd = (self.register(self.confirm), '%P', '%S',
                            'cappuccino')
@@ -829,7 +835,8 @@ class CoffeeShopPage(tk.Frame):
         self.espresso_label = ttk.Label(self, text="Espresso", font=MEDIUM_FONT)
         self.espresso_label.grid(row=15, column=1, columnspan=5, pady=10,
                                  sticky="W")
-        self.espresso_price = ttk.Label(self, text="$3.00", font=MEDIUM_FONT)
+        self.espresso_price = ttk.Label(self, font=MEDIUM_FONT,
+                                        text="${:.2f}".format(self.espre_cost))
         self.espresso_price.grid(row=15, column=4, columnspan=3, pady=10)
         espresso_vcmd = (self.register(self.confirm), '%P', '%S', 'espresso')
         self.espresso_amount = ttk.Entry(self, validate="key", justify="center",
@@ -840,7 +847,8 @@ class CoffeeShopPage(tk.Frame):
         self.flat_w_label = ttk.Label(self, text="Flat White", font=MEDIUM_FONT)
         self.flat_w_label.grid(row=16, column=1, columnspan=5, pady=10,
                                sticky="W")
-        self.flat_w_price = ttk.Label(self, text="$3.00", font=MEDIUM_FONT)
+        self.flat_w_price = ttk.Label(self, font=MEDIUM_FONT,
+                                      text="${:.2f}".format(self.flatw_cost))
         self.flat_w_price.grid(row=16, column=4, columnspan=3, pady=10)
         flat_w_vcmd = (self.register(self.confirm), '%P', '%S', 'flat_white')
         self.flat_w_amount = ttk.Entry(self, validate="key", justify="center",
@@ -851,7 +859,8 @@ class CoffeeShopPage(tk.Frame):
         self.latte_label = ttk.Label(self, text="Latte", font=MEDIUM_FONT)
         self.latte_label.grid(row=17, column=1, columnspan=5, pady=10,
                               sticky="W")
-        self.latte_price = ttk.Label(self, text="$3.00", font=MEDIUM_FONT)
+        self.latte_price = ttk.Label(self, font=MEDIUM_FONT,
+                                     text="${:.2f}".format(self.latte_cost))
         self.latte_price.grid(row=17, column=4, columnspan=3, pady=10)
         latte_vcmd = (self.register(self.confirm), '%P', '%S', 'latte')
         self.latte_amount = ttk.Entry(self, validate="key", justify="center",
@@ -862,7 +871,8 @@ class CoffeeShopPage(tk.Frame):
         self.mocha_label = ttk.Label(self, text="Mocha", font=MEDIUM_FONT)
         self.mocha_label.grid(row=18, column=1, columnspan=5, pady=10,
                               sticky="W")
-        self.mocha_price = ttk.Label(self, text="$3.00", font=MEDIUM_FONT)
+        self.mocha_price = ttk.Label(self, font=MEDIUM_FONT,
+                                     text="${:.2f}".format(self.mocha_cost))
         self.mocha_price.grid(row=18, column=4, columnspan=3, pady=10)
         mocha_vcmd = (self.register(self.confirm), '%P', '%S', 'mocha')
         self.mocha_amount = ttk.Entry(self, validate="key", justify="center",
@@ -870,11 +880,14 @@ class CoffeeShopPage(tk.Frame):
         self.mocha_amount.grid(row=18, column=10, columnspan=2, pady=10)
 
         # Total
-        menu_label = ttk.Label(self, text="Total:", font=MEDIUM_FONT)
-        menu_label.grid(row=20, column=1, sticky="W")
-        self.total_label = ttk.Label(self, text="$0.00", font=MEDIUM_FONT)
-        self.total_label.grid(row=20, column=1, columnspan=6,
-                              padx=10, sticky="E")
+        total_label = ttk.Label(self, text="Total:", font=MEDIUM_FONT)
+        total_label.grid(row=20, column=1, sticky="W")
+        self.total_cost = 0.00
+        self.total_cost_label = ttk.Label(self, font=MEDIUM_FONT,
+                                          text="${:.2f}".format(self.total_cost)
+                                          )
+        self.total_cost_label.grid(row=20, column=1, columnspan=6,
+                                   padx=10, sticky="E")
 
         self.toplevel = None
 
@@ -893,6 +906,8 @@ class CoffeeShopPage(tk.Frame):
                                     command=buy_window, state='disabled')
         self.buy_button.grid(row=20, column=7, columnspan=6, sticky="E", pady=5)
         self.buy_button.image = self.buy_img
+
+        CoffeeShopPage.reset_order_data()
 
     def erase(self):
         """
@@ -928,7 +943,6 @@ class CoffeeShopPage(tk.Frame):
         else:
             try:
                 inserted_value = int(inserted_value)
-                print("Input accepted.")
             except ValueError:
                 print("Input is not an integer!")
         if isinstance(inserted_value, int) and 0 < len(allowed_value) == 1:
@@ -954,9 +968,33 @@ class CoffeeShopPage(tk.Frame):
                 data['total'] = total + data[_type]
                 current_data.append('{}:{}'.format(_type, data[_type]))
                 current_data.append('{}:{}'.format('total', data['total']))
+
+                cappuccino_total = 0
+                espresso_total = 0
+                flat_w_total = 0
+                latte_total = 0
+                mocha_total = 0
+                for item in current_data:
+                    item = item.split(':')
+                    if item[0] == 'cappuccino' and int(item[1]) > 0:
+                        cappuccino_total = int(item[1]) * self.cappu_cost
+                    elif item[0] == 'espresso' and int(item[1]) > 0:
+                        espresso_total = int(item[1]) * self.espre_cost
+                    elif item[0] == 'flat_white' and int(item[1]) > 0:
+                        flat_w_total = int(item[1]) * self.flatw_cost
+                    elif item[0] == 'latte' and int(item[1]) > 0:
+                        latte_total = int(item[1]) * self.latte_cost
+                    elif item[0] == 'mocha' and int(item[1]) > 0:
+                        mocha_total = int(item[1]) * self.mocha_cost
+                new_total = cappuccino_total + espresso_total + \
+                    flat_w_total + latte_total + mocha_total
+
             if int(data['total']) > 0:
+                self.total_cost_label.configure(text="${:.2f}"
+                                                .format(new_total))
                 self.buy_button.configure(state='normal')
             else:
+                self.total_cost_label.configure(text="$0.00")
                 self.buy_button.configure(state='disabled')
             with open(COFFEE_DATA_F, 'w') as file:
                 print("Writing new data to {}".format(COFFEE_DATA_F))
@@ -971,11 +1009,15 @@ class CoffeeShopPage(tk.Frame):
         self.controller.show_frame(ShopPage)
 
     def submit_button(self):
-        username = self.toplevel.username.get()
-        password = self.toplevel.password.get()
+        try:
+            username = self.toplevel.username.get()
+            password = self.toplevel.password.get()
+        except AttributeError:
+            username = "Guest"
+            password = "None"
+
         name_and_pass = username + ',' + password
-        amount = 1
-        print(name_and_pass)
+        amount = float(self.total_cost_label.cget("text").strip('$'))
         if name_and_pass == ',':
             print("Nothing was entered!")
             self.toplevel.user_info.configure(text="Nothing entered!",
@@ -996,10 +1038,14 @@ class CoffeeShopPage(tk.Frame):
             return False
 
     def purchase(self, username, amount):
-        print("AMOUNT: ", amount)
-        if User.withdraw(username, amount) is True:
+        if username == 'Guest':
             self.toplevel.user_info.configure(text="Transaction successful",
                                               foreground="green")
+            CoffeeShopPage.reset_order_data()
+        elif User.withdraw(username, amount) is True:
+            self.toplevel.user_info.configure(text="Transaction successful",
+                                              foreground="green")
+            CoffeeShopPage.reset_order_data()
         elif User.withdraw(username, amount) == "insufficient_funds":
             self.toplevel.user_info.configure(text="Insufficient funds",
                                               foreground="red")
@@ -1007,6 +1053,17 @@ class CoffeeShopPage(tk.Frame):
             self.toplevel.user_info.configure(text="Transaction failed",
                                               foreground="red")
         self.toplevel.user_info.after(2500, lambda: self.remove_window())
+
+    @staticmethod
+    def reset_order_data():
+        """ Resets the COFFEE_DATA_F file. """
+        order_data = ['cappuccino', 'espresso', 'flat_white', 'latte', 'mocha',
+                      'total']
+        new_data = []
+        with open(COFFEE_DATA_F, 'w') as file:
+            for data in order_data:
+                new_data.append('{}:0'.format(data))
+            file.write('\n'.join(new_data))
 
     def order(self):
         if self.toplevel is None:
@@ -1030,39 +1087,63 @@ class CoffeeShopPage(tk.Frame):
             amount_label.grid(row=2, column=20, pady=10)
 
             data = {}
-            with open(CURRENT_DATA_F, 'r') as file:
+            with open(COFFEE_DATA_F, 'r') as file:
                 count = 2
                 for line in file:
                     option, value = line.strip().split(':')
                     data[option] = value
                     if int(data[option]) > 0:
-                        price = "$" + str(int(data[option]) * 3) + ".00"
                         if option == 'flat_white':
-                            coffee_type = 'flat white'
+                            coffee_type = 'Flat White'
+                            price = "${:.2f}".format(int(data[option]) *
+                                                     self.flatw_cost)
+                        elif option == 'cappuccino':
+                            coffee_type = option.title()
+                            price = "${:.2f}".format(int(data[option]) *
+                                                     self.cappu_cost)
+                        elif option == 'espresso':
+                            coffee_type = option.title()
+                            price = "${:.2f}".format(int(data[option]) *
+                                                     self.espre_cost)
+                        elif option == 'latte':
+                            coffee_type = option.title()
+                            price = "${:.2f}".format(int(data[option]) *
+                                                     self.latte_cost)
+                        elif option == 'mocha':
+                            coffee_type = option.title()
+                            price = "${:.2f}".format(int(data[option]) *
+                                                     self.mocha_cost)
+                        elif option == 'total':
+                            coffee_type = option.title()
+                            price = self.total_cost_label.cget("text")
                         else:
                             coffee_type = option
+                            price = ""
+
                         count += 1
-                        ttk.Label(self.toplevel, text=coffee_type.title())\
+                        ttk.Label(self.toplevel, text=coffee_type)\
                             .grid(row=count, column=0, columnspan=10)
                         ttk.Label(self.toplevel, text=data[option])\
                             .grid(row=count, column=10, columnspan=9)
                         ttk.Label(self.toplevel, text=price)\
                             .grid(row=count, column=20)
 
-            # Entries
-            username_label = ttk.Label(self.toplevel, text="Username:",
-                                       font=SMALL_FONT)
-            username_label.grid(row=9, column=7, columnspan=11, pady=5)
-            self.toplevel.username = ttk.Entry(self.toplevel)
-            self.toplevel.username.grid(row=10, column=9, columnspan=12,
-                                        sticky='W', padx=20)
+            with open(current_user_file, 'r') as file:
+                if 'Guest,None,50,1000000' not in file:
+                    # Entries
+                    username_label = ttk.Label(self.toplevel, text="Username:",
+                                               font=SMALL_FONT)
+                    username_label.grid(row=9, column=7, columnspan=11, pady=5)
+                    self.toplevel.username = ttk.Entry(self.toplevel)
+                    self.toplevel.username.grid(row=10, sticky='W', padx=20,
+                                                column=9, columnspan=12)
 
-            password_label = ttk.Label(self.toplevel, text="Password:",
-                                       font=SMALL_FONT)
-            password_label.grid(row=11, column=7, columnspan=11, pady=5)
-            self.toplevel.password = ttk.Entry(self.toplevel, show="*")
-            self.toplevel.password.grid(row=12, column=9, columnspan=12,
-                                        sticky="E", padx=20)
+                    password_label = ttk.Label(self.toplevel, text="Password:",
+                                               font=SMALL_FONT)
+                    password_label.grid(row=11, column=7, columnspan=11, pady=5)
+                    self.toplevel.password = ttk.Entry(self.toplevel, show="*")
+                    self.toplevel.password.grid(row=12, sticky="E", padx=20,
+                                                column=9, columnspan=12)
 
             submit_command = (lambda: self.submit_button())
             cancel_command = (lambda: self.remove_window())
@@ -1391,7 +1472,7 @@ class User:
     def withdraw(username, amount):
         """ Withdraws money from the user's current balance. """
         user_balance = User.get_data(username)['balance']
-        if int(user_balance) >= amount:
+        if float(user_balance) >= amount:
             user_data = list(Check.file()['user_data'])
             password = User.get_data(username)['password']
             age = User.get_data(username)['age']
@@ -1400,7 +1481,7 @@ class User:
                 user_balance
             user_data.remove(del_user)
 
-            new_balance = str(int(user_balance) - amount)
+            new_balance = str(float(user_balance) - amount)
 
             new_user = username + ',' + password + ',' + age + ',' + \
                 new_balance
