6b5b1fa575d691bd59209cfe5d7e5e9123b9aae0
Joshua Booth <joshb00th@icloud.com>
2017-09-23 12:51:21 +1200

    _log addition

---
+++ _log.py
@@ -0,0 +1,44 @@
+# ===================================
+# Filename: _log.py
+# Purpose: To provide logging functionality to the virtual-world program.
+#
+#
+# virtual-world
+# Copyright (C) 2017  Joshua Peter Booth
+#
+# This file is part of virtual-world.
+#
+# virtual-world is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# virtual-world is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with My Library (see LICENCE.md).
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# ===================================
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+log_format = '%(asctime)s - %(levelname)s - %(funcName)s - %(message)s - ' \
+             'Line: %(lineno)d'
+formatter = logging.Formatter(log_format)
+
+fh = logging.FileHandler('log.txt')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
